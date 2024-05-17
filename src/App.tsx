6152353eb001ad58ad1d0f96aad1671a3f037a89
Marco <marco.jucker@students.fhnw.ch>
2024-05-17 11:48:02 +0200

    Added Modal window component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import './App.css'
 import {useState} from "react";
 import {Semester} from "../types/types.ts";
 import {Link} from "react-router-dom";
+import Modal from "@/components/modal.tsx";
 
-function App() {
+export default function App() {
+  localStorage.grade = JSON.stringify(4);
+  localStorage.title = JSON.stringify("B.Sc. Informatik");
   localStorage.semester = JSON.stringify([]);
+  const [grade, setGrade] = useState<number>(JSON.parse(localStorage.grade));
+  const [title, setTitle] = useState<string>(JSON.parse(localStorage.title));
   const [semester, setSemester] = useState<Semester[]>(JSON.parse(localStorage.semester));
+  const [showModal, setShowModal] = useState(false);
 
   function addSemester() {
     const names = [...semester, {name: "Neues Semester", modules: []}];
@@ -17,16 +23,30 @@ function App() {
   return (
     <>
       <div>
+        <h1>{grade} Gewinnt</h1>
+        <h1>{title}</h1>
         <div>
           {semester.map((sem, index) => (
-            <button type="button" key={index}>{sem.name}</button>
+            <Link to="/semester">
+              <button type="button" key={index}>{sem.name}</button>
+            </Link>
           ))}
         </div>
         <button type="button" onClick={addSemester}>+</button>
+        <button
+          className=""
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          Open regular modal
+        </button>
+        <Modal open={showModal} onClose={() => setShowModal(false)}>
+          <div className="text-center w-56">
+            <h3>Modal</h3>
+            <p>Modal</p>
+          </div>
+        </Modal>
       </div>
-      <Link to="/semester">Semester</Link>
     </>
   )
 }
-
-export default App
